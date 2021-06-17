8ab2fa2118fe4cf75f61f3d3a73a10e11b65dd01
Nguyễn Đình Hiếu <DinhHieu8896@gmail.com>
2021-06-17 08:36:21 +0700

    Initial: Practice_IASF

---
+++ src/main/java/com/example/Practice_IASF/PracticeIasfApplication.java
@@ -0,0 +1,13 @@
+package com.example.Practice_IASF;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeIasfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeIasfApplication.class, args);
+	}
+
+}
